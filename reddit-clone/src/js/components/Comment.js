b63b78dfb3772df5cbee0cd6bb218e3637944d78
Kajoban <kuhapark@mcmaster.ca>
2019-05-23 11:37:58 -0400

    structure basically done

--- reddit-clone/src/js/components/Comment.js
+++ reddit-clone/src/js/components/Comment.js
@@ -3,24 +3,22 @@ import React from 'react';
 const Comment = (props) => {
     let title = props.child.data.title || props.child.data.link_title
     let link = `www.reddit.com${props.child.data.permalink}`
-    return(
+    let subreddit = `www.reddit.com${props.child.data.subreddit}`
+    return (
         <div>
             {
-            !props.child.data.body 
-            ?
-            <div>
-                <p></p>
-            </div> 
-            :
-            <div>
-
-                <p>"{props.child.data.body}"</p>
-                <a href='links' target='_blank'>
-                    <p>Reply to "{title}" on r/{props.child.data.subreddit}. </p>
-                </a>
-                <p>{props.child.data.ups} upvotes.</p>
-                <br/>
-            </div> 
+                !props.child.data.body
+                    ?
+                    <div>
+                        <p></p>
+                    </div>
+                    :
+                    <div>
+                        <p>"{props.child.data.body}"</p>
+                        <p> Reply to <a href={link} target='_blank'>"{title}" </a> on <a href={subreddit}>r/{props.child.data.subreddit}</a></p>
+                        <p>{props.child.data.ups} <i class="fas fa-arrow-up"></i>'s</p>
+                        <br />
+                    </div>
             }
         </div>
     )
