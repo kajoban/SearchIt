cc1df175a0dd8c708992bb82d0fad5f2441a138a
Kajoban <kuhapark@mcmaster.ca>
2019-05-24 15:01:35 -0400

    rename

--- search-it/src/js/components/Comment.js
+++ search-it/src/js/components/Comment.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Comment = (props) => {
     let title = props.child.data.title || props.child.data.link_title
-    let link = `www.reddit.com${props.child.data.permalink}`
-    let subreddit = `www.reddit.com${props.child.data.subreddit}`
+    let link = `https://www.reddit.com${props.child.data.permalink}`
+    let subreddit = `https://www.reddit.com/r/${props.child.data.subreddit}`
+    console.log(link, subreddit)
     return (
         <div className='comment'>
             {
@@ -14,7 +15,7 @@ const Comment = (props) => {
                     </div>
                     :
                     <div>
-                        <p className='replyto'> Reply to <a href={link} target='_blank'>"{title}" </a> on <a href={subreddit}>r/{props.child.data.subreddit}</a></p>
+                        <p className='replyto'> Reply to <a href={link} target='_blank'>"{title}" </a> on <a href={subreddit} target='_blank'>r/{props.child.data.subreddit}</a></p>
                         <p className='reply'>"{props.child.data.body}"</p>
                         <p className='upvotes'>{props.child.data.ups} <i className="fas fa-arrow-up"></i>'s</p>
                         <br />
