e5dd17f2b19411e88877d19d37c44947b47eb427
Kajoban <kuhapark@mcmaster.ca>
2019-05-24 14:31:05 -0400

    added limit dropdown

--- reddit-clone/src/js/components/SearchBox.js
+++ reddit-clone/src/js/components/SearchBox.js
@@ -13,11 +13,13 @@ class SearchBox extends React.Component{
         super();
         this.state = {
             searchTerm: '',
-            sort:''
+            sort:'',
+            limit:''
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
-        this.handleDropdownChange = this.handleDropdownChange.bind(this);
+        this.handleSortDropdownChange = this.handleSortDropdownChange.bind(this);
+        this.handleLimitDropdownChange = this.handleLimitDropdownChange.bind(this);
         this.makeLink = this.makeLink.bind(this);
     }
 
@@ -27,9 +29,15 @@ class SearchBox extends React.Component{
         })
     }
 
-    handleDropdownChange(event){
+    handleSortDropdownChange(event){
         this.setState({
             sort: event.target.value
+        })
+    }
+
+    handleLimitDropdownChange(event){
+        this.setState({
+            limit: event.target.value
         })
     }
 
@@ -49,18 +57,23 @@ class SearchBox extends React.Component{
     }
 
     makeLink(){
-        return `http://www.reddit.com/user/${this.state.searchTerm}/overview/.json?sort=${this.state.sort}`
+        return `http://www.reddit.com/user/${this.state.searchTerm}/overview/.json?sort=${this.state.sort}&limit=${this.state.limit}`
     }
 
     render(){
         return(
             <div>
                 <input type='text' value={this.state.searchTerm} onChange={this.handleChange}></input>
-                <select defaultValue='hot' onChange={this.handleDropdownChange}>    
+                <select defaultValue='hot' onChange={this.handleSortDropdownChange}>    
                     <option value='hot'>hot</option>
                     <option value='new'>new</option>
                     <option value='top'>top</option>
                     <option value='controversial'>controversial</option>
+                </select>
+                <select defaultValue='25' onChange={this.handleLimitDropdownChange}>    
+                    <option value='25'>25</option>
+                    <option value='50'>50</option>
+                    <option value='100'>100</option>
                 </select>
                 <form onSubmit={this.handleSubmit}>
                     <input type='submit'></input>
